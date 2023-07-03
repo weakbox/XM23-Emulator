96059212f683b4ddeb4c0072862161ccbd3b3a0c
Connor McLeod <cn248485@dal.ca>
2023-07-03 14:19:25 -0300

    Final version before A1 testing.

--- registers.c
+++ registers.c
@@ -4,11 +4,10 @@
 */
 
 #include "header.h"
+#include "instructions.h"
 
-// Shifts address to the right by 1 bit, effectively dividing it by 2. Converts a byte addressed address into a word addressed address.
-#define word(x) (x>>1)
-
-// Prints the contents of the register file. Includes both 8 registers and 8 constants.
+// Prints the contents of the register file.
+// Includes both 8 registers and 8 constants.
 void print_reg(unsigned short regfile[2][8])
 {
 	printf("Printing the current contents of the register file...\n");
@@ -32,72 +31,50 @@ void print_reg(unsigned short regfile[2][8])
 	}
 }
 
-// Loads from memory at the address stored in the source register, and stores into the destination register.
-void load(unsigned short* dest, unsigned short *source, unsigned short prpo, unsigned short dec, unsigned short inc, unsigned short wb)
+// Updates the Program Status Word (PSW) based on the result of execution for a particular instruction.
+// Retrieved from Dr. Larry Hughes (2023).
+void update_psw(unsigned short dest, unsigned short source, unsigned short result, unsigned short wb)
 {
-	switch (prpo)	/* Determine whether to dec/inc the source register pre/post load: */
-	{
-	case PRE:
-		if (wb == WORD)
-		{
-			*source -= 2*dec;
-			*source += 2*inc;
-			mar = *source;
-			bus(mar, &mbr, READ, WORD);
-			*dest = mbr;
-		}
-		else /* wb == BYTE */
-		{
-			*source -= dec;
-			*source += inc;
-			mar = *source;
-			bus(mar, &mbr, READ, BYTE);
-			*dest = mbr;
-		}
-		break;
+	/*
+	 - Update the PSW bits (V, N, Z, C)
+	 - Using source, dest, and result values and whether word or byte
+	 - ADD, ADDC, SUB, and SUBC
+	*/
+	unsigned short carry[2][2][2] = { 0, 0, 1, 0, 1, 0, 1, 1 };
+	unsigned short overflow[2][2][2] = { 0, 1, 0, 0, 0, 0, 1, 0 };
 
-	case POST:
-		if (wb == WORD)
-		{
-			mar = *source;
-			bus(mar, &mbr, READ, WORD);
-			*dest = mbr;
-			*source -= 2*dec;
-			*source += 2*inc;
-		}
-		else /* wb == BYTE */
-		{
-			mar = *source;
-			bus(mar, &mbr, READ, BYTE);
-			*dest = mbr;
-			*source -= dec;
-			*source += inc;
-		}
-		break;
+	unsigned short mss, msd, msr; /* Most significant source, dest, and result bits */
+
+	if (wb == 0)
+	{
+		mss = MSBIT_WORD(source);
+		msd = MSBIT_WORD(dest);
+		msr = MSBIT_WORD(result);
 	}
+	else /* Byte */
+	{
+		mss = MSBIT_BYTE(source);
+		msd = MSBIT_BYTE(dest);
+		msr = MSBIT_BYTE(result);
+		result &= 0x00FF;	/* Mask high byte for 'z' check */
+	}
+
+	/* Carry */
+	psw.carry = carry[mss][msd][msr];
+	/* Zero */
+	psw.zero = (result == 0);
+	/* Negative */
+	psw.negative = (msr == 1);
+	/* oVerflow */
+	psw.overflow = overflow[mss][msd][msr];
 }
 
-// Takes a high and low byte and combines them into a 2-byte (16-bit) value which is inserted into the given register.
-void move_bytes(unsigned short *reg, unsigned char high, unsigned char low)
+// Prints the current contents of the PSW.
+void print_psw()
 {
-	*reg = (high << 8) | low;
-}
-
-// Loads memory from the address specified by the source register, but with an added offset.
-void load_rel(unsigned short *dest, unsigned short source, short offset, unsigned short wb)
-{
-	switch (wb)
-	{
-	case WORD:
-		mar = source + offset;
-		bus(mar, &mbr, READ, WORD);
-		*dest = mbr;
-		break;
-
-	case BYTE:
-		mar = source + offset;
-		bus(mar, &mbr, READ, BYTE);
-		*dest = mbr;
-		break;
-	}
+	printf("Printing the status of the PSW:\n");
+	printf("Carry:    %i\n", psw.carry);
+	printf("Zero:	  %i\n", psw.zero);
+	printf("Negative: %i\n", psw.negative);
+	printf("Overflow: %i\n", psw.overflow);
 }
